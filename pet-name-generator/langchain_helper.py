aa761a2c238cb48e90874ed78b243a6b21635cf6
Athsara Fernando <athsarafernando470@gmail.com>
2023-12-13 20:32:15 +0530

    agent version

--- pet-name-generator/langchain_helper.py
+++ pet-name-generator/langchain_helper.py
@@ -1,6 +1,9 @@
 from langchain.llms import OpenAI 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,5 +23,15 @@ def generate_pet_name(animal_type, pet_color):
     response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
     return response
 
+def langchain_agent():
+    llm = OpenAI(temperature=0.1)
+    tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+    agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+    result = agent.run("what is the average age of a dog? Multiply the age by 3")
+
+    print(result)
+
 if __name__=="__main__":
-    print(generate_pet_name('cat','black'))
+    langchain_agent()
+    #print(generate_pet_name('cat','black'))
